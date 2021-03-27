e3e6030918adb682de1e6bf6d2a1f5c6d1d6b7df
mike.bijl <mike.bijl@tangent.works>
2021-03-27 13:25:31 +0100

    CSVServer: feat: script that creates csv file & file is served

---
+++ CSVServe/CreateCSV.js
@@ -0,0 +1,46 @@
+const { createWriteStream } = require('fs')
+const { unparse } = require('papaparse')
+const faker = require('faker')
+
+const writeStream = createWriteStream(`${__dirname}/public/test.csv`)
+
+// https://stackoverflow.com/questions/46671199/how-to-add-1-utc-day-to-javascript-date
+const ONE_DAY_TIMESTAMP = 1440 * 60 * 1000
+const NUMBER_OF_ROWS = 1000000
+
+function* makeDate(start=1, end=NUMBER_OF_ROWS, step=1) {
+    let iterationCount = 0
+    for (let i = start; i < end; i+= step) {
+        iterationCount++
+        yield new Date(i * ONE_DAY_TIMESTAMP)
+    }
+    return iterationCount
+}
+
+const dateIterator = makeDate()
+
+function getData() {
+    const data = []
+    for (const date of dateIterator) {
+        data.push({
+            date,
+            value: (Math.random() * 100).toFixed(2),
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            address: faker.address.streetAddress(),
+        })
+    }
+    return data
+}
+
+(async () => {
+    console.log('Creating fake data...')
+    const data = await getData()
+    console.log('Creating Done')
+    console.log('Parsing to csv...')
+    const csv = await unparse(data)
+    console.log('Parsing done')
+    console.log('Writing to file...')
+    await writeStream.write(csv)
+    console.log(`Writing done 🚀 you're good to go`)
+})()
